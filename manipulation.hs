a7e227ce91a5137857c61afb4f7ef337ec1ffca8
Mason Marche <masonmarche@gmail.com>
2019-01-08 10:04:33 -0500

    added new haskell function

--- manipulation.hs
+++ manipulation.hs
@@ -5,3 +5,6 @@ manip i s
 
 bous :: [String] -> [String]
 bous ss = map (\(i, s) -> manip i s) (zip [0..] ss)
+
+test :: String -> [String]
+test s = if (length s < 20) then [s] else (take 20 s : test (drop 20 s))
